efe1267ffd99cd8c51ffc213a13ce9e7c8063fef
hiai <519177109@qq.com>
2016-01-24 14:07:19 +0800

    a

---
+++ app/src/main/java/com/example/administrator/clickalike/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.administrator.clickalike;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
+
+    Animation animation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.btn);
+        animation= AnimationUtils.loadAnimation(this, R.anim.anim1);
+        button.setOnClickListener(this);
+
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        button.startAnimation(animation);
+    }
+}
